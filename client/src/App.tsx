8f6192a9dc7233aec1224c8567322b47f386b321
rohitsolanki2134 <rohitsolanki2134@users.noreply.github.com>
2026-05-29 16:58:03 +0530

    fix: correct import casing for Multiplayer page (Linux case-sensitive build)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SinglePlayer from './pages/SinglePlayer';
-import Multiplayer from './pages/MultiPlayer';
+import Multiplayer from './pages/Multiplayer';
 import Leaderboard from './pages/Leaderboard';
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
